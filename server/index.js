ee0b11fd81a8a72268666c53c11465746aa97a07
Beyakim <beyakimchi0@gmail.com>
2026-01-05 11:04:01 +0200

    fix: bind to Render PORT

--- server/index.js
+++ server/index.js
@@ -111,6 +111,5 @@ app.post("/notes/clear", (req, res) => {
 });
 
 // מפעיל את השרת ומאזין לפורט 3000
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
